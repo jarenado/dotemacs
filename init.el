2f5d9ed48df8d7abac4499e6526568b2283b9cb1
Jose Arenado <jose@unisonagency.com>
2016-02-02 17:37:07 -0500

    added use package and magit

--- init.el
+++ init.el
@@ -1,8 +1,19 @@
+(require 'package)
+(package-initialize)
+(setq package-enable-at-startup nil)
+
+(add-to-list 'load-path "~/.emacs_d/config")
 (setq package-archives '(("melpa" . "http://melpa.milkbox.net/packages/")
                          ("org" . "http://orgmode.org/elpa/")
                          ("gnu" . "http://elpa.gnu.org/packages/")))
 
-(add-to-list 'load-path "~/.emacs_d/config")
+(unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
+(require 'use-package)
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
 
 (setq backup-directory-alist '(("." . "~/.emacs.d/emacs-backup"))
   backup-by-copying t    ; Don't delink hardlinks
@@ -12,8 +23,6 @@
   kept-old-versions 5    ; and how many of the old
   )
 
-(require 'package)
-(package-initialize)
 
 (require 'elisp-slime-nav)
 (defun my-lisp-hook ()
@@ -30,6 +39,25 @@
   (add-hook 'emacs-lisp-mode-hook 'my-lisp-hook)
 )
 
+(require 'helm)
+(require 'helm-config)
+(use-package magit
+  :ensure magit
+  :config
+  (progn
+    (evil-set-initial-state 'magit-mode 'normal)
+    (evil-set-initial-state 'magit-status-mode 'normal)
+    (evil-set-initial-state 'magit-diff-mode 'normal)
+    (evil-set-initial-state 'magit-log-mode 'normal)
+    (evil-define-key 'normal magit-mode-map
+        "j" 'magit-goto-next-section
+        "k" 'magit-goto-previous-section)
+    (evil-define-key 'normal magit-log-mode-map
+        "j" 'magit-goto-next-section
+        "k" 'magit-goto-previous-section)
+    (evil-define-key 'normal magit-diff-mode-map
+        "j" 'magit-goto-next-section
+        "k" 'magit-goto-previous-section)))
 
 (require 'evil-leader)
 (global-evil-leader-mode)
@@ -49,6 +77,8 @@
 (evil-leader/set-key "b" 'ibuffer)
 (evil-leader/set-key "w" 'save-buffer)
 (evil-leader/set-key "," 'other-window)
+(evil-leader/set-key "x" 'helm-M-x)
+(evil-leader/set-key "f" 'helm-find-files)
 
 (define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
 (define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
