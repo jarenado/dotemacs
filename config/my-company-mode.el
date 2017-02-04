3a74c05cacd51373bf9141127865a55201376a4f
Jose Arenado <jose@unisonagency.com>
2017-02-04 14:40:34 -0500

    added case-sensitivity config for company-mode

--- config/my-company-mode.el
+++ config/my-company-mode.el
@@ -6,6 +6,8 @@
 
 (setq company-idle-delay 0.2)
 (setq company-selection-wrap-around t)
+(setq company-dabbrev-downcase nil)
+
 (define-key company-active-map [tab] 'company-complete)
 (define-key company-active-map (kbd "C-n") 'company-select-next)
 (define-key company-active-map (kbd "C-p") 'company-select-previous)
